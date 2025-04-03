e26e281e5e1336e104df90406b86edd3118ce689
peaceRyun <dbsrn1110@gmail.com>
2025-04-03 23:42:31 +0900

    모달 열리는 애니메이션 구현.

--- popup/resources/js/script.js
+++ popup/resources/js/script.js
@@ -1,3 +1,4 @@
+//기본 레이어 팝업(근데 모달임)
 const openPopup = () => {
     document.querySelector('.defaultPopupCont').classList.add('active');
 };
@@ -5,3 +6,16 @@ const openPopup = () => {
 const closePopup = () => {
     document.querySelector('.defaultPopupCont').classList.remove('active');
 };
+
+//animation 모달
+const openAniModal = () => {
+    document.querySelector('.animationModalCont').classList.add('active');
+};
+
+const closeAniModal = () => {
+    document.querySelector('.animationModalCont').classList.remove('active');
+};
+
+document.querySelector('.animationModalBtn').addEventListener('click', openAniModal);
+
+document.querySelector('.animationModalWrap .closeBtn').addEventListener('click', closeAniModal);
