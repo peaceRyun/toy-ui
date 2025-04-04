1cb67d465f31b42ee39e4ec12dc361d1fc5fdde9
peaceRyun <dbsrn1110@gmail.com>
2025-04-04 23:00:32 +0900

    기본 아코디언 js 2차 구현.

--- accordion/resources/js/script.js
+++ accordion/resources/js/script.js
@@ -1,7 +1,22 @@
-const openAccordion = () => {
-    document.querySelectorAll('.accordion .accordion-body').forEach((body) => {
-        body.classList.add('active');
-    });
-};
+document.querySelectorAll('.accordion li').forEach((item) => {
+    const accordionHeader = item.querySelector('a');
+    const accordionBody = item.querySelector('.accordion-body');
 
-document.querySelectorAll('.accordion li').forEach((item) => item.addEventListener('click', openAccordion));
+    accordionHeader.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        if (item.classList.contains('disabled')) {
+            return;
+        }
+
+        document.querySelectorAll('.accordion .accordion-body').forEach((body) => {
+            body.classList.remove('active');
+        });
+        document.querySelectorAll('.accordion a').forEach((header) => {
+            header.classList.remove('active');
+        });
+
+        accordionBody.classList.toggle('active');
+        accordionHeader.classList.toggle('active');
+    });
+});
