c691fa4b936bf7fb22bc656187423c5f6a5fca4e
peaceRyun <dbsrn1110@gmail.com>
2025-04-17 15:34:31 +0900

    video 태그 preoload속성도 2초뒤에 metadata로 변경하도록 구현, video click 이벤트일때만 토글로 pause/play 구현.

--- loadingthree/resources/js/script.js
+++ loadingthree/resources/js/script.js
@@ -19,5 +19,17 @@ slides.forEach((slide) => {
 
     setTimeout(() => {
         slide.querySelector('.loading-wrap').classList.remove('on');
+        slide.querySelector('video').preload = 'metadata';
     }, 2000);
+
+    slide.querySelector('video').addEventListener('click', () => {
+        const video = slide.querySelector('video');
+        //간결하게 conse video = e.target 도 가능하다. 대신 e를 매개변수로 또 받아야 한다.
+
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
 });
